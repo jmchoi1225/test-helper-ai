a33d27ff2f379182cdaef65befae97d4d5a20878
jmchoi1225 <jmchoi1234@hotmail.com>
2021-10-04 09:59:16 +0900

    init backend

---
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/TesthelperApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.ajou.da.testhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesthelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesthelperApplication.class, args);
+	}
+
+}
---
+++ backend/testhelper/src/test/java/kr/ac/ajou/da/testhelper/TesthelperApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.ajou.da.testhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesthelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
