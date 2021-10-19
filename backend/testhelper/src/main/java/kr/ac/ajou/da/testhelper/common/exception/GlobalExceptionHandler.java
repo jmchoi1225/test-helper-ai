2f2286881f49084987a7cf8351eadd91ef21ef45
jmchoi1225 <jmchoi1234@hotmail.com>
2021-10-19 13:28:57 +0900

    4-4 handle Exception in GlobalExceptionHandler

--- backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/common/exception/GlobalExceptionHandler.java
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package kr.ac.ajou.da.testhelper.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,8 +11,13 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(ResponseStatusException exception) {
+    @ExceptionHandler(value = ResponseStatusException.class)
+    public ResponseEntity<ErrorResponse> handleResponseStatusException(ResponseStatusException exception) {
         return ResponseEntity.status(exception.getStatus()).body(new ErrorResponse(exception.getReason()));
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(exception.getMessage()));
     }
 }
