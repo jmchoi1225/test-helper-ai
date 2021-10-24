7be63f983ead2f45d04552493ba82c4409413f4b
jmchoi1225 <jmchoi1234@hotmail.com>
2021-10-24 17:12:13 +0900

    0-0 bugfix, added cors mapping for local frontend

--- backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/common/config/AppConfig.java
+++ backend/testhelper/src/main/java/kr/ac/ajou/da/testhelper/common/config/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mobile.device.DeviceHandlerMethodArgumentResolver;
 import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,5 +31,17 @@ public class AppConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(deviceHandlerMethodArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        /*
+        TODO : Local에서 테스트할 수 있도록 임시적으로 만듦. 추후 수정 예정
+            - hostname을 별도로 저장하기
+            - dev 추가
+         */
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
     }
 }
