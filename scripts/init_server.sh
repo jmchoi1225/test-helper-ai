3c8724892adba7d389390dd82292983f73f2ad68
KoYeJoon <yejoon08@ajou.ac.kr>
2021-10-30 15:52:22 +0900

    2-15. init_server.sh 틀린 부분 찾기위한 echo 추가

--- scripts/init_server.sh
+++ scripts/init_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-if [ -d /home/ubuntu/test-helper ]; then
+if [ -d /home/ubuntu/test-helper ]
+then
+    echo "Directory /home/ubuntu/test-helper exists. delete start!s" 
     rm -rf /home/ubuntu/test-helper 
 fi
