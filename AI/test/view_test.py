9c1f335eca611c8d8c5a19f4b0193e3b10888068
KoYeJoon <yejoon08@ajou.ac.kr>
2021-11-28 02:13:43 +0900

    jinja를 통해 시각적으로 두 손 인식 박스를 통해 실제 결과를 확인해볼 수 있도록 view test 추가

--- AI/test/view_test.py
+++ AI/test/view_test.py
@@ -6,7 +6,13 @@ sys.path.extend(["./","../","./src","../src"])
 
 from student_identification.detectText import detect_text
 from student_identification.compareFace import compare_faces
+from hand_detection.yolo import YOLO
 import s3path
+
+from PIL import Image
+import cv2
+import numpy as np
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -47,8 +53,40 @@ def identification_test():
 
 @app.route('/hand-detection',methods=['POST'])
 def temp_detection_test():
-    pass
+    hand_img = Image.open(request.files['hand_img'])
+    hand_img = cv2.cvtColor(np.array(hand_img), cv2.COLOR_RGB2BGR) 
+    width, height, results = detection_model.inference(hand_img)
+    conf_sum = 0
+    detection_count = 0
+    for detection in results:
+        id, name, confidence, x, y, w, h = detection
+        cx = x + (w / 2)
+        cy = y + (h / 2)
+
+        conf_sum += confidence
+        detection_count += 1
+
+        # draw a bounding box rectangle and label on the image
+        color = (255, 0, 255)
+        cv2.rectangle(hand_img, (x, y), (x + w, y + h), color, 3)
+        text = "%s (%s)" % (name, round(confidence, 2))
+        cv2.putText(hand_img, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.25, color, 1)
+
+        print("%s with %s confidence" % (name, round(confidence, 2)))
+
+    cv2.imwrite("static/images/result.jpg", hand_img)
+    # opencvImage = cv2.resize(result_image,dsize=(360,240))
+    # cv2.imwrite("static/images/result.jpg",opencvImage)
+    result_path = "images/result.jpg"
+    # # K.clear_session()
+    return render_template(
+            'result_hand.html', 
+            result_path = result_path,
+            hand_num=detection_count, 
+    )
 
 if __name__ == '__main__':
+    detection_model = YOLO("../src/hand_detection/models/yolov4-tiny-custom.cfg","../src/hand_detection/models/yolov4-tiny-custom_only_egodataset.weights", ["hand"])
     app.run(host='0.0.0.0',port=5000,threaded=True, debug=True)
     #app.run(host='0.0.0.0',port=5000,debug=True)
