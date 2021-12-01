666bcc8a64aa62bdab4f4ff68b6869a58fb01b1c
KoYeJoon <yejoon08@ajou.ac.kr>
2021-12-02 02:47:11 +0900

    test echo 수정

--- AI/test/all_test.sh
+++ AI/test/all_test.sh
@@ -2,14 +2,18 @@
 
 # api Test
 echo API TEST
+echo hand detection TEST
 python AI/test/apiTest/hand_detection_apiTest.py
 echo "\n"
+echo identification TEST
 python AI/test/apiTest/identification_apiTest.py
 
 
 # module Test
 echo "\n\n\n"
 echo MODULE TEST
+echo hand detection TEST
 python AI/test/moduleTest/hand_detection_moduleTest.py
 echo "\n"
+echo identification TEST
 python AI/test/moduleTest/identification_moduleTest.py
